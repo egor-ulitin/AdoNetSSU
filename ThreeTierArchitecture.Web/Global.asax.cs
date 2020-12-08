ad4513a1f619aaa7427fed8c67939b05ab91b9b7
egor-ulitin <55191387+egor-ulitin@users.noreply.github.com>
2020-12-08 17:24:48 +0400

    Add files via upload

---
+++ ThreeTierArchitecture.Web/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using Ninject;
+using Ninject.Modules;
+using Ninject.Web.Mvc;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ThreeTierArchitecture.BLL.Infrastructure;
+using ThreeTierArchitecture.Web.Util;
+
+namespace ThreeTierArchitecture.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            NinjectModule orderModule = new OrderModule();
+            NinjectModule serviceModule = new ServiceModule("DefaultConnection");
+            var kernel = new StandardKernel(orderModule, serviceModule);
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
+    }
+}
